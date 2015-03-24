c5ee386e874e592d885ec38e261391f186e03802
ericktorres1 <ericktorres90.et@gmail.com>
2015-03-24 11:53:02 -0400

    Initial Commit

---
+++ UITableViewController Challenge Solution/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  UITableViewController Challenge Solution
+//
+//  Created by Erick Torres on 3/24/15.
+//  Copyright (c) 2015 Erick Torres. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ UITableViewController Challenge Solution/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  UITableViewController Challenge Solution
+//
+//  Created by Erick Torres on 3/24/15.
+//  Copyright (c) 2015 Erick Torres. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
