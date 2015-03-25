4eceda75333f120fc1076979a51c511c3c034461
ericktorres1 <ericktorres90.et@gmail.com>
2015-03-25 19:03:44 -0400

    Table view added with some content

--- UITableViewController Challenge Solution/ChallengeSolutionTableViewController.h
+++ UITableViewController Challenge Solution/ChallengeSolutionTableViewController.h
@@ -1,5 +1,5 @@
 //
-//  ViewController.h
+//  ChallengeSolutionTableViewController.h
 //  UITableViewController Challenge Solution
 //
 //  Created by Erick Torres on 3/24/15.
@@ -8,8 +8,6 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
-
+@interface ChallengeSolutionTableViewController : UITableViewController
 
 @end
-
